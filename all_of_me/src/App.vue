75b1535496bac0e301f2d7b1d954edebbe4674c8
meteoriteLX <15021762+meteoritelx@user.noreply.gitee.com>
2025-04-19 12:55:19 +0800

    init

---
+++ all_of_me/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import todoList from './components/todoList.vue';
+import {reactive,ref} from 'vue'
+  const List = reactive({
+    todoList:[
+      {id:1,task:'吃饭',isCompleted:true},
+      {id:2,task:'睡觉',isCompleted:false},
+    ]
+  })
+  //其实这种简单的可以不用reactive，可以直接用普通对象or直接数组orref
+</script>
+
+<template>
+  <div id="app">
+    <h1>根组件</h1> 
+    <!-- :propName="value" 将数据传递给子组件。
+子组件通过 defineProps 或 props 选项声明并接收这些数据。 -->
+    <todoList :todoList="List.todoList"></todoList>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ all_of_me/src/components/todoList.vue
@@ -0,0 +1,40 @@
+<script setup>
+  // name: "todoList"
+  defineProps({
+    todoList:{
+      type:Array,
+      required: true,
+      default:[]
+      // ↑默认值为空
+    }
+  })
+
+</script>
+
+<template>
+  <ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center"
+  v-for="item in todoList" :key="item.id">
+    <!-- 复选框 -->
+      <div class="form-check">
+      <input class="form-check-input" type="checkbox"  :id="item.id">
+      <label class="form-check-label" :for="item.id">
+        {{ item.task }}
+      </label>
+    </div>
+    <span class="badge badge-primary badge-pill" v-if="item.isCompleted">已完成</span>
+    <span class="badge badge-warning badge-pill" v-else>未完成</span>
+  </li>
+  
+</ul>
+
+</template>
+
+<style scoped>
+/* scoped 防止不同组件样式之间出问题 */
+  .list-group{
+    width: 500px; 
+    margin: auto;
+
+  }
+</style>
