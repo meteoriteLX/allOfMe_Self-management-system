670fb066b88ce191b4f8b52f7a8f9bf60a0d4bed
meteoriteLX <15021762+meteoritelx@user.noreply.gitee.com>
2025-04-28 18:04:02 +0800

    beautify

--- all_of_me/src/components/home.vue
+++ all_of_me/src/components/home.vue
@@ -64,47 +64,56 @@ import {ref} from 'vue'
       </div>
       <div class="card-section">
         <div class="card">
-          <div class="card-title">首页</div>
-          <div class="card-content">这是首页</div>
+          <div class="card-title"><div style="font-size: 30px;">All Of Me</div> 个人管理</div>
+          <div class="card-content">在这里记录关于“我”的一切。<br> 这里是生活的控制台，梦想的导航仪。<br> 每日清晨，让AllOfMe成为你的能量舱——<br> 看一看全局规划，点一盏专注明灯，在效率与从容间找到完美平衡。<br>
+          <br><span style="font-size: 18px;">This is the control panel of life, the navigator of dreams.Each morning, <br> let the homepage be your energy capsule — scanning the grand plan, lighting the lamp of focus, and finding perfect balance between efficiency and serenity.</span></div>
           <!-- <div class="card-img">
             <img src="" alt="">
           </div> -->
           <div class="btn-box">
-            <button class="main-btn">前往</button>
+            <!-- <button class="main-btn">前往</button> -->
             
             
           </div>
         </div>
         <div class="card">
-          <div class="card-title">待办</div>
-          <div class="card-content">这是待办事项</div>
+          <div class="card-title"><div style="font-size: 30px;">TodoList</div>待办清单</div>
+          <div class="card-content">每一段旅程都始于清晰的足迹，每一个目标都成于有序的规划。<br> 无论是琐碎日常还是宏伟项目，你的待办清单都将化身时光的舵手，助你在纷繁世界中锚定方向，让「未完成」蜕变为「已成就」。<br>
+            <span style="font-size: 18px;">Every journey begins with clear footprints, and every goal thrives on structured plans. From daily chores to grand projects, your to-do list becomes the helmsman of time, anchoring your focus in a chaotic world and transforming "pending" into "accomplished."</span> 
+          </div>
           <!-- <div class="card-img">
             <img src="" alt="">
           </div> -->
           <div class="btn-box">
-            <button class="main-btn">前往</button>
-            <router-link to="/todos" class="nav-btn">待办事项</router-link>
+            <!-- <button class="main-btn">前往</button> -->
+            <router-link to="/todos" class="main-btn">前往待办清单</router-link>
           </div>
         </div>
         <div class="card">
-          <div class="card-title">日历</div>
-          <div class="card-content">这是日历</div>
+          <div class="card-title"><div style="font-size: 30px;">Calendar</div>日历</div>
+          <div class="card-content">时间是最公正的叙事者，而日历是你私人的编年史官。<br> 当晨曦染红日历的边角，你看到的不仅是日期，<br>
+           更是无数个「可能」在晨光中舒展羽翼。 <br>
+          <span style="font-size: 18px;">Time is the fairest storyteller, and the calendar your personal chronicler. As dawn tints the calendar's edges, you behold not just dates but countless "possibilities" stretching their wings in the morning light.
+
+</span></div>
           <!-- <div class="card-img">
             <img src="" alt="">
           </div> -->
           <div class="btn-box">
-            <button class="main-btn">前往</button>
-            <router-link to="/calendar" class="nav-btn">日历视图</router-link>
+            <!-- <button class="main-btn">前往</button> -->
+            <router-link to="/calendar" class="main-btn">前往日历视图</router-link>
           </div>
         </div>
         <div class="card">
-          <div class="card-title">日记</div>
-          <div class="card-content">这是日记</div>
+          <div class="card-title"><div style="font-size: 30px;">Diary</div>日记</div>
+          <div class="card-content">文字是心灵的显影剂，日记是时光的保险箱。<br> 请在这里记录你的生活，你的故事。每一笔记录都是对自我的温柔叩问，每一页空白都静待着用真诚墨水书写的故事——因为最动人的成长，往往藏匿于那些未被说出口的独白。<br>
+            <span style="font-size: 18px;">Words are the developer of the soul, and the diary a time capsule. Every entry is a gentle inquiry to the self, every blank page awaits stories penned with sincere ink — for the most poignant growth often hides in unspoken soliloquies.</span>
+          </div>
           <!-- <div class="card-img">
             <img src="" alt="">
           </div> -->
           <div class="btn-box">
-            <button class="main-btn">前往</button>
+            <!-- <button class="main-btn">前往</button> -->
           </div>
         </div>
       </div>
@@ -143,25 +152,22 @@ import {ref} from 'vue'
   margin-left: 10%;
 
 }
-.drawer-boxes::before{
+/* .drawer-boxes::before{
   content: "";
       width: 8px;
       height: 94%;
       position: absolute;
-      left: -10px;
+      left: -20px;
       top: 3%;
       z-index: 120;
-      
-
-
-}
+} */
 .drawer-box{
   height: calc(100% /5.5);
   width: 70%;
   flex: 0 1 auto; 
   position: relative;
   z-index: 100;
-  transform: translateX(-70%);
+  transform: translateX(-72%);
   transition: transform .5s ease-in-out;
 
 }
@@ -182,8 +188,9 @@ import {ref} from 'vue'
 }
 .drawer-btn.active {
       background-image: url(../assets/img/bg2.jpg);
+      
       background-size: cover;
-      color: rgb(70, 100, 180);
+      color: rgb(86, 117, 204);
 }
 .drawer-head {
       position: absolute;
@@ -248,15 +255,15 @@ import {ref} from 'vue'
 }
 
 .card-title {
-      font-size: 80px;
+      font-size: 50px;
       font-weight: 700;
       padding-bottom: min(20%, 40px);
-      color: rgb(70, 112, 180);
+      color: rgb(88, 116, 206);
 }
 .card-content {
-      font-size: 24px;
+      font-size: 20px;
       font-weight: 400;
-      color: rgb(94, 140, 255);
+      color: rgb(118, 149, 227);
       margin-bottom: 60px;
     }
 
@@ -268,4 +275,7 @@ import {ref} from 'vue'
 .card-img img {
       width: 100%;
     }
+.btn-box {
+  margin: auto;
+}
 </style>
--- all_of_me/src/components/todo.vue
+++ all_of_me/src/components/todo.vue
@@ -38,12 +38,24 @@ const filterTodolist = computed(() =>{
 
 <template>
   <div id="app">
-    <h1>根组件</h1> 
-    <todoInput @addTask="addTask"/>
-    <!-- :propName="value" 将数据传递给子组件。
-子组件通过 defineProps 或 props 选项声明并接收这些数据。 -->
-    <todoList :todoList="filterTodolist" @updateTask="updateTask"></todoList>
-    <todoButton @updateActive="handleUpdateActive"></todoButton>
+    <div class="nav1">
+        <nav>
+        &nbsp;&nbsp;<router-link to="/home" class="nav-title">首页</router-link>
+        &nbsp;<router-link to="/todos" class="nav-title">待办清单</router-link>
+        &nbsp;<router-link to="/calendar" class="nav-title">日历视图</router-link>
+        </nav>
+    </div>
+    
+    <div class="todolist-body">
+      <h1>待办清单</h1> 
+      <h2>TodoList</h2>
+      <todoInput @addTask="addTask"/>
+      <!-- :propName="value" 将数据传递给子组件。
+  子组件通过 defineProps 或 props 选项声明并接收这些数据。 -->
+      <todoList :todoList="filterTodolist" @updateTask="updateTask"></todoList>
+      <todoButton @updateActive="handleUpdateActive"></todoButton>
+      
+    </div>
     
   </div>
 
@@ -51,10 +63,26 @@ const filterTodolist = computed(() =>{
 
 <style scoped>
 #app {
-  text-align: center;
   background-image: url(../assets/img/bg2.jpg);
   height: 100vh;
   width: 100vw;
+  color: rgb(14, 56, 145);
 }
 
+.todolist-body {
+  text-align: center;
+}
+#app h1{
+  font-size: 35px;
+  padding: 20px;
+  color: rgb(51, 91, 177);
+}
+#app h2{
+  font-size: 25px;
+  padding-bottom: 15px;
+  color: rgb(51, 91, 177);
+}
+.nav1 {
+
+}
 </style>
